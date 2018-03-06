89544ce2d7f0e3266a22c63a9685924ddf813620
Oliver Gerlich <oliver.gerlich@gmx.de>
2018-03-06 22:06:19 +0100

    test_check_gatemons.sh: fix shellcheck warnings

--- tools/check_gatemons/test_check_gatemons.sh
+++ tools/check_gatemons/test_check_gatemons.sh
@@ -25,40 +25,40 @@ assert ()
 
 
 # basic test
-res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u file://`pwd`/testdata.json -s vpn01.bremen.freifunk.net=vpn1 -i '_addresses_ipv6$')
+res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u "file://$(pwd)/testdata.json" -s vpn01.bremen.freifunk.net=vpn1 -i '_addresses_ipv6$')
 assert [ "$?" -eq 0 ]
-assert [ "$res" == """GATEMONS OK - 0 criticals, 0 warnings, 7 services checked
+assert [ "$res" == "GATEMONS OK - 0 criticals, 0 warnings, 7 services checked
 vpn1_addresses_ipv4: 0 bad reports (0%)
 vpn1_dns_ipv4: 0 bad reports (0%)
 vpn1_dns_ipv6: 0 bad reports (0%)
 vpn1_ntp_ipv4: 0 bad reports (0%)
 vpn1_ntp_ipv6: 0 bad reports (0%)
 vpn1_uplink_ipv4: 1 bad reports (25%)
-vpn1_uplink_ipv6: 1 bad reports (25%)|'vpn1_addresses_ipv4'=0%;50;80 'vpn1_dns_ipv4'=0%;50;80 'vpn1_dns_ipv6'=0%;50;80 'vpn1_ntp_ipv4'=0%;50;80 'vpn1_ntp_ipv6'=0%;50;80 'vpn1_uplink_ipv4'=25%;50;80 'vpn1_uplink_ipv6'=25%;50;80 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800""" ]
+vpn1_uplink_ipv6: 1 bad reports (25%)|'vpn1_addresses_ipv4'=0%;50;80 'vpn1_dns_ipv4'=0%;50;80 'vpn1_dns_ipv6'=0%;50;80 'vpn1_ntp_ipv4'=0%;50;80 'vpn1_ntp_ipv6'=0%;50;80 'vpn1_uplink_ipv4'=25%;50;80 'vpn1_uplink_ipv6'=25%;50;80 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
 
 
 # test with nonexisting hostname
-res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u file://`pwd`/testdata.json -s nonexisting-hostname 2>/dev/null)
+res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u "file://$(pwd)/testdata.json" -s nonexisting-hostname 2>/dev/null)
 assert [ "$?" -eq 3 ]
 assert [ "$res" == "UNKNOWN (Exception <type 'exceptions.Exception'>: no data available for server \"nonexisting-hostname\")" ]
 
 
 # test without any -s parameter
-res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u file://`pwd`/testdata.json -i '_addresses_' -i '_ntp_' -i '_uplink' -i '_ipv6$' 2>/dev/null)
+res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u "file://$(pwd)/testdata.json" -i '_addresses_' -i '_ntp_' -i '_uplink' -i '_ipv6$' 2>/dev/null)
 assert [ "$?" -eq 2 ]
-assert [ "$res" == """GATEMONS CRITICAL - 2 criticals, 0 warnings, 6 services checked
+assert [ "$res" == "GATEMONS CRITICAL - 2 criticals, 0 warnings, 6 services checked
 vpn01.bremen.freifunk.net_dns_ipv4: 0 bad reports (0%)
 vpn02.bremen.freifunk.net_dns_ipv4: 0 bad reports (0%)
 vpn03.bremen.freifunk.net_dns_ipv4: 0 bad reports (0%)
 vpn04.bremen.freifunk.net_dns_ipv4: 4 bad reports (100%) (CRITICAL)
 vpn05.bremen.freifunk.net_dns_ipv4: 0 bad reports (0%)
-vpn06.bremen.freifunk.net_dns_ipv4: 4 bad reports (100%) (CRITICAL)|'vpn01.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn03.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn04.bremen.freifunk.net_dns_ipv4'=100%;50;80 'vpn05.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn06.bremen.freifunk.net_dns_ipv4'=100%;50;80 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800""" ]
+vpn06.bremen.freifunk.net_dns_ipv4: 4 bad reports (100%) (CRITICAL)|'vpn01.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn03.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn04.bremen.freifunk.net_dns_ipv4'=100%;50;80 'vpn05.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn06.bremen.freifunk.net_dns_ipv4'=100%;50;80 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
 
 
 # test with different -w and -c parameters
-res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u file://`pwd`/testdata.json -s vpn01.bremen.freifunk.net=vpn1 -s vpn02.bremen.freifunk.net -i '_addresses_ipv6$' -w 24 -c 25 2>/dev/null)
+res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u "file://$(pwd)/testdata.json" -s vpn01.bremen.freifunk.net=vpn1 -s vpn02.bremen.freifunk.net -i '_addresses_ipv6$' -w 24 -c 25 2>/dev/null)
 assert [ "$?" -eq 2 ]
-assert [ "$res" == """GATEMONS CRITICAL - 2 criticals, 0 warnings, 14 services checked
+assert [ "$res" == "GATEMONS CRITICAL - 2 criticals, 0 warnings, 14 services checked
 vpn02.bremen.freifunk.net_addresses_ipv4: 0 bad reports (0%)
 vpn02.bremen.freifunk.net_dns_ipv4: 0 bad reports (0%)
 vpn02.bremen.freifunk.net_dns_ipv6: 0 bad reports (0%)
@@ -72,13 +72,13 @@ vpn1_dns_ipv6: 0 bad reports (0%)
 vpn1_ntp_ipv4: 0 bad reports (0%)
 vpn1_ntp_ipv6: 0 bad reports (0%)
 vpn1_uplink_ipv4: 1 bad reports (25%) (CRITICAL)
-vpn1_uplink_ipv6: 1 bad reports (25%) (CRITICAL)|'vpn02.bremen.freifunk.net_addresses_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_dns_ipv6'=0%;24;25 'vpn02.bremen.freifunk.net_ntp_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_ntp_ipv6'=0%;24;25 'vpn02.bremen.freifunk.net_uplink_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_uplink_ipv6'=0%;24;25 'vpn1_addresses_ipv4'=0%;24;25 'vpn1_dns_ipv4'=0%;24;25 'vpn1_dns_ipv6'=0%;24;25 'vpn1_ntp_ipv4'=0%;24;25 'vpn1_ntp_ipv6'=0%;24;25 'vpn1_uplink_ipv4'=25%;24;25 'vpn1_uplink_ipv6'=25%;24;25 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800""" ]
+vpn1_uplink_ipv6: 1 bad reports (25%) (CRITICAL)|'vpn02.bremen.freifunk.net_addresses_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_dns_ipv6'=0%;24;25 'vpn02.bremen.freifunk.net_ntp_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_ntp_ipv6'=0%;24;25 'vpn02.bremen.freifunk.net_uplink_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_uplink_ipv6'=0%;24;25 'vpn1_addresses_ipv4'=0%;24;25 'vpn1_dns_ipv4'=0%;24;25 'vpn1_dns_ipv6'=0%;24;25 'vpn1_ntp_ipv4'=0%;24;25 'vpn1_ntp_ipv6'=0%;24;25 'vpn1_uplink_ipv4'=25%;24;25 'vpn1_uplink_ipv6'=25%;24;25 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
 
 
 # test with different -w and -c parameters
-res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u file://`pwd`/testdata.json -s vpn01.bremen.freifunk.net=vpn1 -s vpn02.bremen.freifunk.net -i '_addresses_ipv6$' -w 25 -c 26 2>/dev/null)
+res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u "file://$(pwd)/testdata.json" -s vpn01.bremen.freifunk.net=vpn1 -s vpn02.bremen.freifunk.net -i '_addresses_ipv6$' -w 25 -c 26 2>/dev/null)
 assert [ "$?" -eq 1 ]
-assert [ "$res" == """GATEMONS WARNING - 0 criticals, 2 warnings, 14 services checked
+assert [ "$res" == "GATEMONS WARNING - 0 criticals, 2 warnings, 14 services checked
 vpn02.bremen.freifunk.net_addresses_ipv4: 0 bad reports (0%)
 vpn02.bremen.freifunk.net_dns_ipv4: 0 bad reports (0%)
 vpn02.bremen.freifunk.net_dns_ipv6: 0 bad reports (0%)
@@ -92,13 +92,13 @@ vpn1_dns_ipv6: 0 bad reports (0%)
 vpn1_ntp_ipv4: 0 bad reports (0%)
 vpn1_ntp_ipv6: 0 bad reports (0%)
 vpn1_uplink_ipv4: 1 bad reports (25%) (WARNING)
-vpn1_uplink_ipv6: 1 bad reports (25%) (WARNING)|'vpn02.bremen.freifunk.net_addresses_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_dns_ipv6'=0%;25;26 'vpn02.bremen.freifunk.net_ntp_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_ntp_ipv6'=0%;25;26 'vpn02.bremen.freifunk.net_uplink_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_uplink_ipv6'=0%;25;26 'vpn1_addresses_ipv4'=0%;25;26 'vpn1_dns_ipv4'=0%;25;26 'vpn1_dns_ipv6'=0%;25;26 'vpn1_ntp_ipv4'=0%;25;26 'vpn1_ntp_ipv6'=0%;25;26 'vpn1_uplink_ipv4'=25%;25;26 'vpn1_uplink_ipv6'=25%;25;26 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800""" ]
+vpn1_uplink_ipv6: 1 bad reports (25%) (WARNING)|'vpn02.bremen.freifunk.net_addresses_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_dns_ipv6'=0%;25;26 'vpn02.bremen.freifunk.net_ntp_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_ntp_ipv6'=0%;25;26 'vpn02.bremen.freifunk.net_uplink_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_uplink_ipv6'=0%;25;26 'vpn1_addresses_ipv4'=0%;25;26 'vpn1_dns_ipv4'=0%;25;26 'vpn1_dns_ipv6'=0%;25;26 'vpn1_ntp_ipv4'=0%;25;26 'vpn1_ntp_ipv6'=0%;25;26 'vpn1_uplink_ipv4'=25%;25;26 'vpn1_uplink_ipv6'=25%;25;26 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
 
 
 # test --max-age: only one gatemon report is less than 3 minutes old
-res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u file://`pwd`/testdata.json -s vpn01.bremen.freifunk.net --max-age 180 2>/dev/null)
+res=$(faketime -f '2018-02-25 22:20:00' ./check_gatemons.py -u "file://$(pwd)/testdata.json" -s vpn01.bremen.freifunk.net --max-age 180 2>/dev/null)
 assert [ "$?" -eq 2 ]
-assert [ "$res" == """GATEMONS CRITICAL - 1 criticals, 0 warnings, 8 services checked
+assert [ "$res" == "GATEMONS CRITICAL - 1 criticals, 0 warnings, 8 services checked
 vpn01.bremen.freifunk.net_addresses_ipv4: 0 bad reports (0%)
 vpn01.bremen.freifunk.net_addresses_ipv6: 1 bad reports (100%) (CRITICAL)
 vpn01.bremen.freifunk.net_dns_ipv4: 0 bad reports (0%)
@@ -106,7 +106,7 @@ vpn01.bremen.freifunk.net_dns_ipv6: 0 bad reports (0%)
 vpn01.bremen.freifunk.net_ntp_ipv4: 0 bad reports (0%)
 vpn01.bremen.freifunk.net_ntp_ipv6: 0 bad reports (0%)
 vpn01.bremen.freifunk.net_uplink_ipv4: 0 bad reports (0%)
-vpn01.bremen.freifunk.net_uplink_ipv6: 0 bad reports (0%)|'vpn01.bremen.freifunk.net_addresses_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_addresses_ipv6'=100%;50;80 'vpn01.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_dns_ipv6'=0%;50;80 'vpn01.bremen.freifunk.net_ntp_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_ntp_ipv6'=0%;50;80 'vpn01.bremen.freifunk.net_uplink_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_uplink_ipv6'=0%;50;80 'gatemon_gatemon1'=107s;;180 'gatemon_gatemon2'=403s;;180 'gatemon_gatemon3'=1540s;;180 'gatemon_gatemon4'=399s;;180""" ]
+vpn01.bremen.freifunk.net_uplink_ipv6: 0 bad reports (0%)|'vpn01.bremen.freifunk.net_addresses_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_addresses_ipv6'=100%;50;80 'vpn01.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_dns_ipv6'=0%;50;80 'vpn01.bremen.freifunk.net_ntp_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_ntp_ipv6'=0%;50;80 'vpn01.bremen.freifunk.net_uplink_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_uplink_ipv6'=0%;50;80 'gatemon_gatemon1'=107s;;180 'gatemon_gatemon2'=403s;;180 'gatemon_gatemon3'=1540s;;180 'gatemon_gatemon4'=399s;;180" ]
 
 
 echo "$numTestsFailed tests failed"
