5aba75be515b567e3be3faa59416b9ef4a002a9c
mortzu <mortzu@uni-bremen.de>
2019-04-28 18:23:24 +0200

    Show version on gatemon page

--- app.js
+++ app.js
@@ -16,15 +16,15 @@ $(function() {
   // Parse response
   function parseResponse(data) {
     // Reset counter for array
-    meshmon_counter = 0;
+    gatemon_counter = 0;
 
-    // Iterate over meshmons
-    data.forEach(function(meshmon) {
+    // Iterate over gatemons
+    data.forEach(function(gatemon) {
       // Increment counter
-      meshmon_counter++;
+      gatemon_counter++;
 
-      // Iterate over meshmon data
-      meshmon['vpn-servers'].forEach(function(vpnserver_data) {
+      // Iterate over gatemon data
+      gatemon['vpn-servers'].forEach(function(vpnserver_data) {
         // Short VPN hostname
         vpnserver_name = vpnserver_data['name'].split('.')[0];
 
@@ -33,30 +33,30 @@ $(function() {
           $('<div class="col-lg-6 col-md-12"><div class="well"><table class="table" id="' + vpnserver_name + '"><thead><tr id="' + vpnserver_name + 'server"></tr><tr id="' + vpnserver_name + 'services"><td></td></tr><tr id="' + vpnserver_name + 'servicesfamily"><td></td></tr></thead><tbody></tbody></table></div></div>').appendTo($('#content'));
         }
 
-        $('<tr id="' + vpnserver_name + meshmon['uuid'] + '"><td title="Name: ' + meshmon['name'] + '\nProvider: ' + meshmon['provider'] + '\nZuletzt aktualisiert: ' + meshmon['lastupdated'] + '">' + meshmon['name'] + '</td></tr>').appendTo($('#' + vpnserver_name + ' tbody'));
+        $('<tr id="' + vpnserver_name + gatemon['uuid'] + '"><td title="Name: ' + gatemon['name'] + '\nProvider: ' + gatemon['provider'] + '\nVersion: ' + gatemon['version'] + '\nZuletzt aktualisiert: ' + gatemon['lastupdated'] + '">' + gatemon['name'] + '</td></tr>').appendTo($('#' + vpnserver_name + ' tbody'));
 
-        // Iterate over services returned by meshmon
+        // Iterate over services returned by gatemon
         counter = 0;
         for (var key in vpnserver_data) {
           // Check if item is an array
           if (Object.prototype.toString.call(vpnserver_data[key]) === '[object Array]') {
             counter++;
-            if (meshmon_counter <= 1) {
+            if (gatemon_counter <= 1) {
               $('<td colspan="2" class="text-center">' + key + '</td>').appendTo($('#' + vpnserver_name + 'services'));
               $('<td class="text-center">IPv4</td><td class="text-center">IPv6</td>').appendTo($('#' + vpnserver_name + 'servicesfamily'));
             }
 
             $.each(['ipv4', 'ipv6'], function() {
               if (vpnserver_data[key][0][this]) {
-                $('<td class="good"></td>').appendTo($('#' + vpnserver_name + meshmon['uuid']));
+                $('<td class="good"></td>').appendTo($('#' + vpnserver_name + gatemon['uuid']));
               } else {
-                $('<td class="bad"></td>').appendTo($('#' + vpnserver_name + meshmon['uuid']));
+                $('<td class="bad"></td>').appendTo($('#' + vpnserver_name + gatemon['uuid']));
               }
             });
           }
         }
 
-        if (meshmon_counter <= 1) {
+        if (gatemon_counter <= 1) {
           $('<th colspan="' + (2 * counter + 1) + '" class="text-center">' + vpnserver_data['name'] + '</th>').appendTo($('#' + vpnserver_name + 'server'));
         }
       });
