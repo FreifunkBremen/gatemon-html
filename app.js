88738fb7a91e894a6679df6ea556d708c508dbf9
mortzu <mortzu@gmx.de>
2019-07-26 22:40:32 +0200

    Show node informations

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ $(function() {
       +'<tr>'
       + '<th>Gatemon Name</th>'
       + '<th>Provider</th>'
+      + '<th>Node</th>'
       + '<th>Version</th>'
       + '<th>Last Update</time></th>'
       + '</tr>'
@@ -45,6 +46,7 @@ $(function() {
       $('<tr>'
         + '<td>' + gatemon['name'] + '</td>'
         + '<td>' + gatemon['provider'] + '</td>'
+        + '<td>' + ((gatemon['node-id'] != undefined) ? '<a href="https://map.bremen.freifunk.net/#!/map/' + gatemon['node-id'] + '">' + gatemon['node-hostname'] + '</a></td>' : '')
         + '<td>' + gatemon['version'] + '</td>'
         + '<td class="' + gatemon_class + '"><time class="timeago" datetime="' + gatemon['lastupdated'] + '">' + gatemon['lastupdated'] + '</time></td>'
         + '</tr>').appendTo($('#lastupdated'));
