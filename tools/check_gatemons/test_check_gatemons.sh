268ad3a491c1af90742bf419e3fe6c14701b51f1
Oliver Gerlich <oliver.gerlich@gmx.de>
2022-01-24 19:48:48 +0100

    test_check_gatemons.sh: test that gatemon names with Umlauts are correctly handled

--- tools/check_gatemons/test_check_gatemons.sh
+++ tools/check_gatemons/test_check_gatemons.sh
@@ -34,7 +34,7 @@ vpn1_dns_ipv6: 0 bad reports (0%)
 vpn1_ntp_ipv4: 0 bad reports (0%)
 vpn1_ntp_ipv6: 0 bad reports (0%)
 vpn1_uplink_ipv4: 1 bad reports (25%)
-vpn1_uplink_ipv6: 1 bad reports (25%)|'vpn1_addresses_ipv4'=0%;50;80 'vpn1_dns_ipv4'=0%;50;80 'vpn1_dns_ipv6'=0%;50;80 'vpn1_ntp_ipv4'=0%;50;80 'vpn1_ntp_ipv6'=0%;50;80 'vpn1_uplink_ipv4'=25%;50;80 'vpn1_uplink_ipv6'=25%;50;80 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
+vpn1_uplink_ipv6: 1 bad reports (25%)|'vpn1_addresses_ipv4'=0%;50;80 'vpn1_dns_ipv4'=0%;50;80 'vpn1_dns_ipv6'=0%;50;80 'vpn1_ntp_ipv4'=0%;50;80 'vpn1_ntp_ipv6'=0%;50;80 'vpn1_uplink_ipv4'=25%;50;80 'vpn1_uplink_ipv6'=25%;50;80 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2_umlauts_äöüß_test'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
 
 
 # test with nonexisting hostname
@@ -52,7 +52,7 @@ vpn02.bremen.freifunk.net_dns_ipv4: 0 bad reports (0%)
 vpn03.bremen.freifunk.net_dns_ipv4: 0 bad reports (0%)
 vpn04.bremen.freifunk.net_dns_ipv4: 4 bad reports (100%) (CRITICAL)
 vpn05.bremen.freifunk.net_dns_ipv4: 0 bad reports (0%)
-vpn06.bremen.freifunk.net_dns_ipv4: 4 bad reports (100%) (CRITICAL)|'vpn01.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn03.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn04.bremen.freifunk.net_dns_ipv4'=100%;50;80 'vpn05.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn06.bremen.freifunk.net_dns_ipv4'=100%;50;80 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
+vpn06.bremen.freifunk.net_dns_ipv4: 4 bad reports (100%) (CRITICAL)|'vpn01.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn03.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn04.bremen.freifunk.net_dns_ipv4'=100%;50;80 'vpn05.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn06.bremen.freifunk.net_dns_ipv4'=100%;50;80 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2_umlauts_äöüß_test'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
 
 
 # test with different -w and -c parameters
@@ -72,7 +72,7 @@ vpn1_dns_ipv6: 0 bad reports (0%)
 vpn1_ntp_ipv4: 0 bad reports (0%)
 vpn1_ntp_ipv6: 0 bad reports (0%)
 vpn1_uplink_ipv4: 1 bad reports (25%) (CRITICAL)
-vpn1_uplink_ipv6: 1 bad reports (25%) (CRITICAL)|'vpn02.bremen.freifunk.net_addresses_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_dns_ipv6'=0%;24;25 'vpn02.bremen.freifunk.net_ntp_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_ntp_ipv6'=0%;24;25 'vpn02.bremen.freifunk.net_uplink_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_uplink_ipv6'=0%;24;25 'vpn1_addresses_ipv4'=0%;24;25 'vpn1_dns_ipv4'=0%;24;25 'vpn1_dns_ipv6'=0%;24;25 'vpn1_ntp_ipv4'=0%;24;25 'vpn1_ntp_ipv6'=0%;24;25 'vpn1_uplink_ipv4'=25%;24;25 'vpn1_uplink_ipv6'=25%;24;25 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
+vpn1_uplink_ipv6: 1 bad reports (25%) (CRITICAL)|'vpn02.bremen.freifunk.net_addresses_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_dns_ipv6'=0%;24;25 'vpn02.bremen.freifunk.net_ntp_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_ntp_ipv6'=0%;24;25 'vpn02.bremen.freifunk.net_uplink_ipv4'=0%;24;25 'vpn02.bremen.freifunk.net_uplink_ipv6'=0%;24;25 'vpn1_addresses_ipv4'=0%;24;25 'vpn1_dns_ipv4'=0%;24;25 'vpn1_dns_ipv6'=0%;24;25 'vpn1_ntp_ipv4'=0%;24;25 'vpn1_ntp_ipv6'=0%;24;25 'vpn1_uplink_ipv4'=25%;24;25 'vpn1_uplink_ipv6'=25%;24;25 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2_umlauts_äöüß_test'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
 
 
 # test with different -w and -c parameters
@@ -92,7 +92,7 @@ vpn1_dns_ipv6: 0 bad reports (0%)
 vpn1_ntp_ipv4: 0 bad reports (0%)
 vpn1_ntp_ipv6: 0 bad reports (0%)
 vpn1_uplink_ipv4: 1 bad reports (25%) (WARNING)
-vpn1_uplink_ipv6: 1 bad reports (25%) (WARNING)|'vpn02.bremen.freifunk.net_addresses_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_dns_ipv6'=0%;25;26 'vpn02.bremen.freifunk.net_ntp_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_ntp_ipv6'=0%;25;26 'vpn02.bremen.freifunk.net_uplink_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_uplink_ipv6'=0%;25;26 'vpn1_addresses_ipv4'=0%;25;26 'vpn1_dns_ipv4'=0%;25;26 'vpn1_dns_ipv6'=0%;25;26 'vpn1_ntp_ipv4'=0%;25;26 'vpn1_ntp_ipv6'=0%;25;26 'vpn1_uplink_ipv4'=25%;25;26 'vpn1_uplink_ipv6'=25%;25;26 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
+vpn1_uplink_ipv6: 1 bad reports (25%) (WARNING)|'vpn02.bremen.freifunk.net_addresses_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_dns_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_dns_ipv6'=0%;25;26 'vpn02.bremen.freifunk.net_ntp_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_ntp_ipv6'=0%;25;26 'vpn02.bremen.freifunk.net_uplink_ipv4'=0%;25;26 'vpn02.bremen.freifunk.net_uplink_ipv6'=0%;25;26 'vpn1_addresses_ipv4'=0%;25;26 'vpn1_dns_ipv4'=0%;25;26 'vpn1_dns_ipv6'=0%;25;26 'vpn1_ntp_ipv4'=0%;25;26 'vpn1_ntp_ipv6'=0%;25;26 'vpn1_uplink_ipv4'=25%;25;26 'vpn1_uplink_ipv6'=25%;25;26 'gatemon_gatemon1'=107s;;28800 'gatemon_gatemon2_umlauts_äöüß_test'=403s;;28800 'gatemon_gatemon3'=1540s;;28800 'gatemon_gatemon4'=399s;;28800" ]
 
 
 # test --max-age: only one gatemon report is less than 3 minutes old
@@ -106,7 +106,7 @@ vpn01.bremen.freifunk.net_dns_ipv6: 0 bad reports (0%)
 vpn01.bremen.freifunk.net_ntp_ipv4: 0 bad reports (0%)
 vpn01.bremen.freifunk.net_ntp_ipv6: 0 bad reports (0%)
 vpn01.bremen.freifunk.net_uplink_ipv4: 0 bad reports (0%)
-vpn01.bremen.freifunk.net_uplink_ipv6: 0 bad reports (0%)|'vpn01.bremen.freifunk.net_addresses_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_addresses_ipv6'=100%;50;80 'vpn01.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_dns_ipv6'=0%;50;80 'vpn01.bremen.freifunk.net_ntp_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_ntp_ipv6'=0%;50;80 'vpn01.bremen.freifunk.net_uplink_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_uplink_ipv6'=0%;50;80 'gatemon_gatemon1'=107s;;180 'gatemon_gatemon2'=403s;;180 'gatemon_gatemon3'=1540s;;180 'gatemon_gatemon4'=399s;;180" ]
+vpn01.bremen.freifunk.net_uplink_ipv6: 0 bad reports (0%)|'vpn01.bremen.freifunk.net_addresses_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_addresses_ipv6'=100%;50;80 'vpn01.bremen.freifunk.net_dns_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_dns_ipv6'=0%;50;80 'vpn01.bremen.freifunk.net_ntp_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_ntp_ipv6'=0%;50;80 'vpn01.bremen.freifunk.net_uplink_ipv4'=0%;50;80 'vpn01.bremen.freifunk.net_uplink_ipv6'=0%;50;80 'gatemon_gatemon1'=107s;;180 'gatemon_gatemon2_umlauts_äöüß_test'=403s;;180 'gatemon_gatemon3'=1540s;;180 'gatemon_gatemon4'=399s;;180" ]
 
 
 echo "$numTestsFailed tests failed"
